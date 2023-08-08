4dbe791e4f970dabab20a16e99db2489532edee7
sakovich.v <sakovich.vitali22@gmail.com>
2023-08-08 18:10:29 +0300

    add

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,11 @@
 Fancybox.bind("[data-fancybox]", {});
+
+var swiper = new Swiper(".clients-slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
