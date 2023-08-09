8ce5bbe5260d780705ed77094c269de5e8763e11
vitali <sakovich199622@gmail.com>
2023-08-09 23:33:10 +0300

    add

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,51 +1,149 @@
+Fancybox.bind("[data-fancybox]", {});
+
+var swiper = new Swiper(".clients-slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+// события на инпут
 (() => {
-    Fancybox.bind("[data-fancybox]", {});
+    function inputEvent(label) {
+        const input = label.querySelector(".js-input");
+        const clearBtn = label.querySelector(".js-clear-input");
 
-    (() => {
-        var swiper = new Swiper(".clients-slider", {
-            slidesPerView: 3,
-            spaceBetween: 30,
-            loop: true,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
-    })();
-
-    // события на инпут
-    (() => {
-        function inputEvent(label) {
-            const input = label.querySelector(".js-input");
-            const clearBtn = label.querySelector(".js-clear-input");
-
-            function inputValueСheck() {
-                if (input.value != "") {
-                    label.classList.add("label--is-active");
-                } else {
-                    label.classList.remove("label--is-active");
-                }
-            }
-
-            function inputFocus() {
+        function inputValueСheck() {
+            if (input.value != "") {
                 label.classList.add("label--is-active");
+            } else {
+                label.classList.remove("label--is-active");
             }
-
-            input.addEventListener("input", inputValueСheck);
-            input.addEventListener("focus", inputFocus);
-            input.addEventListener("blur", inputValueСheck);
-
-            if (clearBtn) {
-                clearBtn.addEventListener("click", () => {
-                    input.value = "";
-                    inputValueСheck();
-                });
-            }
-
-            inputValueСheck();
         }
 
-        const label = document.querySelectorAll(".js-label");
-        label.forEach(inputEvent);
-    })();
+        function inputFocus() {
+            label.classList.add("label--is-active");
+        }
+
+        input.addEventListener("input", inputValueСheck);
+        input.addEventListener("focus", inputFocus);
+        input.addEventListener("blur", inputValueСheck);
+
+        if (clearBtn) {
+            clearBtn.addEventListener("click", () => {
+                input.value = "";
+                inputValueСheck();
+            });
+        }
+
+        inputValueСheck();
+    }
+
+    const label = document.querySelectorAll(".js-label");
+    label.forEach(inputEvent);
 })();
+
+// фокусировка табом
+function formTab() {
+    const formModal = document.querySelectorAll(".popup form");
+    formModal.forEach(function (formModalItem) {
+        let lastElem = formModalItem.querySelector(".last-elem");
+        let firstElem = formModalItem.querySelector(".first-elem");
+
+        if (!lastElem || !firstElem) {
+            return;
+        }
+
+        lastElem.onkeydown = function (e) {
+            if (e.key == "Tab" && !e.shiftKey) {
+                firstElem.focus();
+                return false;
+            }
+        };
+
+        firstElem.onkeydown = function (e) {
+            if (e.key == "Tab" && e.shiftKey) {
+                lastElem.focus();
+                return false;
+            }
+        };
+        firstElem.focus();
+    });
+}
+
+// открытие модалок
+function openModal(modal) {
+    modal.style.display = "flex";
+    formTab();
+    document.body.classList.add("hidden");
+    modal.classList.add("popup--show");
+}
+
+// закрытие модалок
+function modal(modal) {
+    const overlay = modal.querySelector(".popup__overlay");
+    const closeModal = modal.querySelectorAll(".popup__close");
+
+    if (!overlay) {
+        return;
+    }
+    closeModal.forEach(function (closeModalItem) {
+        closeModalItem.addEventListener("click", modalHidden);
+    });
+
+    overlay.addEventListener("click", modalHidden);
+
+    function modalHidden() {
+        modal.classList.remove("popup--show");
+        document.body.classList.remove("hidden");
+        window.setTimeout(function () {
+            modal.style.display = "none";
+        }, 300);
+    }
+}
+const modals = document.querySelectorAll(".popup");
+modals.forEach(modal);
+
+// вызов модалок
+const globalClickHandlers = {
+    "js-open-modal": (node) =>
+        openModal(document.querySelector(node.dataset.openModal)),
+};
+document.addEventListener("click", function (e) {
+    var foundNodes = [];
+    var checkRecursive = (target) => {
+        if (target === document || target == undefined) {
+            return false;
+        }
+        var cl = target.classList;
+
+        if (cl === undefined) {
+            return false;
+        }
+        var contains = false;
+        for (var c of Object.keys(globalClickHandlers)) {
+            if (cl.contains(c)) {
+                contains = true;
+                break;
+            }
+        }
+        if (contains) {
+            foundNodes.push(target);
+        }
+
+        return checkRecursive(target.parentElement);
+    };
+    checkRecursive(e.target);
+
+    var handlers = Object.entries(globalClickHandlers);
+    foundNodes.map((node) => {
+        handlers.map(([className, callback]) => {
+            if (node.classList.contains(className)) {
+                callback(node);
+            }
+        });
+    });
+});
