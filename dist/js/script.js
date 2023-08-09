88039486a9ca45156e5a1fcce3f3f277d07fd746
sakovich.v <sakovich.vitali22@gmail.com>
2023-08-09 12:02:12 +0300

    add

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,33 @@
-Fancybox.bind("[data-fancybox]", {});
+(() => {
+    Fancybox.bind("[data-fancybox]", {});
 
-var swiper = new Swiper(".clients-slider", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    }
-});
+    (() => {
+        var swiper = new Swiper(".clients-slider", {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            loop: true,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+    })();
+
+    // очистка инпута
+    (() => {
+        function clearInput(clearInput) {
+            const input = clearInput.querySelector(".js-input");
+            const clearBtn = clearInput.querySelector(".js-clear-input");
+
+            if (!clearBtn) {
+                return;
+            }
+
+            clearBtn.addEventListener("click", function (e) {
+                input.value = "";
+            });
+        }
+        const label = document.querySelectorAll(".js-label");
+        label.forEach(clearInput);
+    })();
+})();
