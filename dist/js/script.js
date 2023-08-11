86374e078e7b2dd345d68b66073ec3e910afcb65
sakovich.v <sakovich.vitali22@gmail.com>
2023-08-11 17:18:44 +0300

    add adaptive

--- dist/js/script.js
+++ dist/js/script.js
@@ -179,3 +179,86 @@ document.addEventListener("click", function (e) {
         }
     });
 })();
+
+(() => {
+    class ItcTabs {
+        constructor(target, config) {
+            const defaultConfig = {};
+            this._config = Object.assign(defaultConfig, config);
+            this._elTabs =
+                typeof target === "string"
+                    ? document.querySelector(target)
+                    : target;
+            this._elButtons = this._elTabs.querySelectorAll(".tabs__btn");
+            this._elPanes = this._elTabs.querySelectorAll(".tabs__pane");
+            this._eventShow = new Event("tab.itc.change");
+            this._init();
+            this._events();
+        }
+        _init() {
+            this._elTabs.setAttribute("role", "tablist");
+            this._elButtons.forEach((el, index) => {
+                el.dataset.index = index;
+                el.setAttribute("role", "tab");
+                this._elPanes[index].setAttribute("role", "tabpanel");
+            });
+        }
+        show(elLinkTarget) {
+            const elPaneTarget = this._elPanes[elLinkTarget.dataset.index];
+            const elLinkActive =
+                this._elTabs.querySelector(".tabs__btn--active");
+            const elPaneShow = this._elTabs.querySelector(".tabs__pane--show");
+            if (elLinkTarget === elLinkActive) {
+                return;
+            }
+            elLinkActive
+                ? elLinkActive.classList.remove("tabs__btn--active")
+                : null;
+            elPaneShow ? elPaneShow.classList.remove("tabs__pane--show") : null;
+            elLinkTarget.classList.add("tabs__btn--active");
+            elPaneTarget.classList.add("tabs__pane--show");
+            this._elTabs.dispatchEvent(this._eventShow);
+            elLinkTarget.focus();
+        }
+        showByIndex(index) {
+            const elLinkTarget = this._elButtons[index];
+            elLinkTarget ? this.show(elLinkTarget) : null;
+        }
+        _events() {
+            this._elTabs.addEventListener("click", (e) => {
+                const target = e.target.closest(".tabs__btn");
+                if (target) {
+                    e.preventDefault();
+                    this.show(target);
+                }
+            });
+        }
+    }
+
+    new ItcTabs(".tabs");
+})();
+
+(() => {
+    const products = document.querySelector(".products");
+    let selectedItem;
+
+    products.onclick = function (event) {
+        let item = event.target.closest(".products__item");
+
+        if (!item) return;
+
+        if (!products.contains(item)) return;
+
+        classAdd(item);
+    };
+
+    function classAdd(node) {
+        if (selectedItem) {
+            selectedItem.classList.remove("products__item--active");
+        }
+        selectedItem = node;
+        selectedItem.classList.add("products__item--active");
+        const block = node.querySelector(".products__subbrend");
+        node.style.setProperty("--height-block", block.scrollHeight + "px");
+    }
+})();
