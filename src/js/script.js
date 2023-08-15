672a66aebe8a42955d97720eb871ce37cbdb7258
vitali <sakovich199622@gmail.com>
2023-08-16 00:16:26 +0300

    add contacts

--- src/js/script.js
+++ src/js/script.js
@@ -65,6 +65,69 @@ var swiper = new Swiper(".clients-slider", {
     label.forEach(inputEvent);
 })();
 
+(() => {
+    window.addEventListener("DOMContentLoaded", function () {
+        [].forEach.call(
+            document.querySelectorAll(".js-input-mask"),
+            function (input) {
+                var keyCode;
+                function mask(event) {
+                    event.keyCode && (keyCode = event.keyCode);
+                    var pos = this.selectionStart;
+                    if (pos < 3) event.preventDefault();
+                    var matrix = "+7 (___) ___-__-__",
+                        i = 0,
+                        def = matrix.replace(/\D/g, ""),
+                        val = this.value.replace(/\D/g, ""),
+                        new_value = matrix.replace(/[_\d]/g, function (a) {
+                            return i < val.length ? val.charAt(i++) : a;
+                        });
+                    i = new_value.indexOf("_");
+                    if (i != -1) {
+                        i < 5 && (i = 3);
+                        new_value = new_value.slice(0, i);
+                    }
+                    var reg = matrix
+                        .substr(0, this.value.length)
+                        .replace(/_+/g, function (a) {
+                            return "\\d{1," + a.length + "}";
+                        })
+                        .replace(/[+()]/g, "\\$&");
+                    reg = new RegExp("^" + reg + "$");
+                    if (
+                        !reg.test(this.value) ||
+                        this.value.length < 5 ||
+                        (keyCode > 47 && keyCode < 58)
+                    ) {
+                        this.value = new_value;
+                        if (this.classList.contains("js-input-mask")) {
+                            this.classList.remove("not");
+                        }
+                    }
+                    if (event.type == "blur" && this.value.length < 5) {
+                        this.classList.add("not");
+                    }
+
+                    console.log(this.value);
+                }
+
+                input.addEventListener("input", mask, false);
+                input.addEventListener("focus", mask, false);
+                input.addEventListener("blur", mask, false);
+                input.addEventListener("keydown", mask, false);
+            }
+        );
+    });
+
+    const formHandle = document.querySelectorAll(".form");
+
+    formHandle.forEach((formItem) => {
+        new Validator(formItem, function (err, res) {
+            return res;
+        });
+    });
+})();
+
 // фокусировка табом
 function formTab() {
     const formModal = document.querySelectorAll(".popup form");
